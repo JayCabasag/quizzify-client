037308a59633e29094f9403c5b9d5211513c1e6a
Jay Cabasag <jaycabasag1999@gmail.com>
2023-06-20 22:06:50 +0800

    Added initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { RootComponent } from './root/root.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RootComponent, title: 'root' },
+  {
+    path: 'auth', component: AuthComponent, title: 'auth', children: [
+      { path: 'signin', component: SigninComponent, title: 'signin' },
+      { path: 'signup', component: SignupComponent, title: 'signup' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { RootComponent } from './root/root.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    RootComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
